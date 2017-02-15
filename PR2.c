8c5c1ac6b2ff1bef48ebdf632b6b21b5bfbbee70
Plotnikov Alexandr <Sashu998@gmail.com>
2017-02-15 23:30:54 +0700

    Conclude final amount

--- PR2.c
+++ PR2.c
@@ -2,40 +2,41 @@
 
 int main()
 {
-int a;
-long int b;
-printf("enter the inital sum of more than 10000:");
-scanf("%li",&b);
-printf("\n");
+	int a;
+	long int b;
+	printf("enter the inital sum of more than 10000:");
+	scanf("%li",&b);
+	printf("\n");
 
-printf("enter the number of days less than a year:");
-scanf("%d",&a);
-printf("\n");
+	printf("enter the number of days less than a year:");
+	scanf("%d",&a);
+	printf("\n");
 
-if (b<100000)
-{
-if (a>=0 && a<=30)
-b=b-(b*10/100);
-if (a >= 31 && a <= 120) 
-b = b + (b * 2 / 100); 
-if (a >= 121 && a < 240) 
-b = b + (b * 6 / 100); 
-if (a >= 241 && a <= 365) 
-b = b + (b * 12 / 100); 
-}
+	if (b<100000)
+	{
+		if (a>=0 && a<=30)
+			b=b-(b*10/100);
+		if (a >= 31 && a <= 120) 
+			b = b + (b * 2 / 100); 
+		if (a >= 121 && a < 240) 
+			b = b + (b * 6 / 100); 
+		if (a >= 241 && a <= 365) 
+			b = b + (b * 12 / 100); 
+	}
 
-else 
-{ 
-if (a >= 0 && a <= 30) 
-b = b - (b * 10 / 100); 
-if (a >= 31 && a <= 120)
-b = b + (b * 3 / 100); 
-if (a >= 121 && a < 240) 
-b = b + (b * 8 / 100); 
-if (a >= 241 && a <= 365) 
-b = b + (b * 15 / 100); 
-} 
+	else 
+	{ 
+		if (a >= 0 && a <= 30) 
+			b = b - (b * 10 / 100); 
+		if (a >= 31 && a <= 120)
+			b = b + (b * 3 / 100); 
+		if (a >= 121 && a < 240) 
+			b = b + (b * 8 / 100); 
+		if (a >= 241 && a <= 365) 
+			b = b + (b * 15 / 100); 
+	} 
 
+	printf("summa za god: %li \n",b); 
 
-return (0);
+	return (0);
 }
