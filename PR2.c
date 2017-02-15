d6233ca2eaa5c94a33e7735f58209051f473f550
Plotnikov Alexandr <Sashu998@gmail.com>
2017-02-15 23:25:05 +0700

    Realization calculating the income of the deposit is less than 100,000

--- PR2.c
+++ PR2.c
@@ -12,5 +12,17 @@ printf("enter the number of days less than a year:");
 scanf("%d",&a);
 printf("\n");
 
+if (b<100000)
+{
+if (a>=0 && a<=30)
+b=b-(b*10/100);
+if (a >= 31 && a <= 120) 
+b = b + (b * 2 / 100); 
+if (a >= 121 && a < 240) 
+b = b + (b * 6 / 100); 
+if (a >= 241 && a <= 365) 
+b = b + (b * 12 / 100); 
+}
+
 return (0);
 }
