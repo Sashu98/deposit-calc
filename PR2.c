ce606b54eeadb5c398ee3ce77761f3d3ed882901
Plotnikov Alexandr <Sashu998@gmail.com>
2017-02-15 23:19:50 +0700

    Input and filling of variables

--- PR2.c
+++ PR2.c
@@ -2,6 +2,15 @@
 
 int main()
 {
+int a;
+long int b;
+printf("enter the inital sum of more than 10000:");
+scanf("%li",&b);
+printf("\n");
+
+printf("enter the number of days less than a year:");
+scanf("%d",&a);
+printf("\n");
 
 return (0);
 }
